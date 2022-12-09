c36d9b179f77f794e771a391c88958fb3f9936fd
Prabhas Prasad <71103824+red123-abc@users.noreply.github.com>
2022-12-09 20:29:35 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
 
-let yourArray;
+let yourArray = ['some string', 21, true, 'another string', 'one more string'];
 // assign your array the correct value in the next line
 // your code here!
 // code end
